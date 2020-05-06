9d25d5d8024de48b8488bb69e0aae5435386bc81
mmcguire91 <mmcguire91@gmail.com>
2020-05-06 11:49:14 -0400

    Play on I am rich original iOS app

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.cyan,
+        appBar: AppBar(
+          title: Center(
+            child: Text('I am poor'),
+          ),
+          backgroundColor: Colors.blueGrey,
+        ),
+        body: Center(
+          child: Image(
+            image: AssetImage('images/icons8-coal-80.png'),
+          ),
+        ),
+      ),
+    ),
+  );
+}
